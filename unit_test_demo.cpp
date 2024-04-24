97cb82232cad4d57156f46fa68fe7df58838c8f9
Anwaar Hadi <anwaar.hadi@gmail.com>
2024-04-24 15:40:14 -0500

    update to unit test demo

--- unit_test_demo.cpp
+++ unit_test_demo.cpp
@@ -14,6 +14,7 @@ TEST_CASE("Test of Sorting Algorithms", "[sort]") {
     std::array<int, N> not_sorted = {5, 4, 3, 2, 1};
     std::array<int, N> sorted = {1, 2, 3, 4, 5};
     REQUIRE(is_sorted(sorted));
+    REQUIRE(!is_sorted(not_sorted));
     REQUIRE(bubble_sort(not_sorted) == sorted);
     REQUIRE(insertion_sort(not_sorted) == sorted);
     REQUIRE(selection_sort(not_sorted) == sorted);
